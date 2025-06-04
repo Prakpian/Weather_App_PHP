aa5b5ea085021c2b8979aacefc7e13115b30a5a2
Prakpian <143757147+Prakpian@users.noreply.github.com>
2025-06-04 21:00:26 +0100

    display weather data

--- fetch_api.php
+++ fetch_api.php
@@ -15,15 +15,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         die("Error: Failed to fetch data from the API.");
     }
 
-    $result = json_decode($response, true);
+    $geocode = json_decode($response, true);
 
     if (json_last_error() !== JSON_ERROR_NONE) {
         die("Error: Invalid API response.");
     }
 
-    header('Content-Type: application/json');
-    echo json_encode($result, JSON_PRETTY_PRINT);
+    if (!isset($geocode['results'][0])) {
+        die("Error: unknown city");
+    }
+
+    $latitude = $geocode['results'][0]['latitude'];
+    $longitude = $geocode['results'][0]['longitude'];
+
+    $weatherDataURL = "https://api.open-meteo.com/v1/forecast?latitude=" . urlencode($latitude) . "&longitude=" . urlencode($longitude) . "&current=temperature_2m";
+    $weatherResponse = file_get_contents($weatherDataURL);
+    $weatherData = json_decode($weatherResponse, true);
+    $weatherTemperature = $weatherData["current"]["temperature_2m"];
+
+    echo "<h2>$city</h2><br/><p>$weatherTemperature °C</p>";
+
 } else {
     die("Error: This script only accepts POST requests.");
 }
-?>
